be9856de7ad9c1e92db2e036478dd815236242d5
erfanvaredi <erfanvaredi@outlook.com>
2024-06-22 20:44:10 +0400

    chore: Update version to 0.0.2-beta in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='logfinance',
-    version='0.0.1',
+    version='0.0.2-beta',
     packages=find_packages(),
     install_requires=[],
     author='Erfan Varedi',
